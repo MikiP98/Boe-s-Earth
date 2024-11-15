7dc6ed9d09da686bf52243a512a970048ed842a7
MikiP98 <pokora.mikolaj@gmail.com>
2024-11-15 16:16:41 +0100

    Initial commit with empty template

---
+++ src/client/java/io/github/mikip98/boesearth/BoesEarthClient.java
@@ -0,0 +1,10 @@
+package io.github.mikip98.boesearth;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BoesEarthClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
