295020b73d7195de3f475d2c106465a19b712c55
MikiP98 <pokora.mikolaj@gmail.com>
2024-11-17 23:06:27 +0100

    Added `is_on_leaves` blockstate to carpet blocks

---
+++ src/main/java/io/github/mikip98/boesearth/mixin/BlockMixinForCarpetMixin.java
@@ -0,0 +1,40 @@
+package io.github.mikip98.boesearth.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Block.class)
+public abstract class BlockMixinForCarpetMixin {
+    @Shadow
+    private BlockState defaultState;
+
+    @Unique
+    protected final void setDefaultState(BlockState state) {
+        this.defaultState = state;
+    }
+
+    @Inject(at = @At("HEAD"), method = "appendProperties")
+    public void addBlockState(CallbackInfo info, @Local(argsOnly = true) StateManager.Builder<Block, BlockState> builder) {}
+
+    @Inject(at = @At("TAIL"), method = "<init>")
+    public void correctDefaultState(CallbackInfo info) {}
+
+    @Inject(at = @At("RETURN"), method = "getPlacementState", cancellable = true)
+    public void modifyPlacementState(ItemPlacementContext ctx, CallbackInfoReturnable<BlockState> cir) {}
+
+//    @Inject(at = @At("HEAD"), method = "randomTick")
+//    private void fixStateOnRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {}
+}
---
+++ src/main/java/io/github/mikip98/boesearth/mixin/CarpetMixin.java
@@ -0,0 +1,70 @@
+package io.github.mikip98.boesearth.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import io.github.mikip98.boesearth.blockstates.IsOnLeaves;
+import io.github.mikip98.boesearth.config.ModConfig;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.CarpetBlock;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.registry.tag.BlockTags;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.StateManager;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.gen.Invoker;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(CarpetBlock.class)
+public class CarpetMixin extends BlockMixinForCarpetMixin {
+    @Override
+    public void addBlockState(CallbackInfo info, @Local(argsOnly = true) StateManager.Builder<Block, BlockState> builder) {
+        builder.add(IsOnLeaves.IS_ON_LEAVES);
+    }
+
+    @Override
+    public void correctDefaultState(CallbackInfo info) {
+        CarpetBlock new_this = (CarpetBlock) (Object) this;
+        setDefaultState(new_this.getDefaultState().withIfExists(IsOnLeaves.IS_ON_LEAVES, false));
+    }
+
+    @Override
+    public void modifyPlacementState(ItemPlacementContext ctx, CallbackInfoReturnable<BlockState> cir) {
+        if (ModConfig.snowOnLeavesBlockstate) {
+            boolean isOnLeaves = checkIfOnLeaves(ctx.getWorld(), ctx.getBlockPos());
+            cir.setReturnValue(cir.getReturnValue().withIfExists(IsOnLeaves.IS_ON_LEAVES, isOnLeaves));
+        }
+    }
+
+//    @Override
+//    public void fixStateOnRandomTick(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo ci) {
+//        if (ModConfig.correctSnowWithTime && !state.get(IsOnLeaves.IS_ON_LEAVES)) {
+//            if (ModConfig.snowOnLeavesBlockstate) {
+//                boolean isOnLeaves = checkIfOnLeaves(world, pos);
+//                world.setBlockState(pos, state.with(IsOnLeaves.IS_ON_LEAVES, isOnLeaves));
+//            } else {
+//                world.setBlockState(pos, state.with(IsOnLeaves.IS_ON_LEAVES, false));
+//            }
+//        }
+//    }
+
+    @Unique
+    private static boolean checkIfOnLeaves(World world, BlockPos pos) {
+        BlockState blockBelow = world.getBlockState(pos.down());
+        boolean isOnLeaves = blockBelow.isIn(BlockTags.LEAVES);
+
+        if (!isOnLeaves) {
+            isOnLeaves = blockBelow.getOrEmpty(IsOnLeaves.IS_ON_LEAVES).orElse(false);
+        }
+
+        return isOnLeaves;
+    }
+}
